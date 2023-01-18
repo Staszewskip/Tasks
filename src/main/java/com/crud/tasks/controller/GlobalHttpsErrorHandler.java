bbcbfef0d710a5eb51f28eb8314b4338a75327fb
Pawel Staszewski <p.staszewski1@gmail.com>
2023-01-18 17:18:49 +0100

    19

---
+++ src/main/java/com/crud/tasks/controller/GlobalHttpsErrorHandler.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalHttpsErrorHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<Object> handleTaskNotFoundException(TaskNotFoundException exception) {
+        return new ResponseEntity<>("Task with given id not exists", HttpStatus.BAD_REQUEST);
+    }
+}
--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -2,10 +2,19 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
 @AllArgsConstructor
+@Getter
+
 public class TaskDto {
+
     private Long id;
     private String title;
     private String content;
